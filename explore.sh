89392b345f43524f376ece6d9d43323d74c17e16
misaka-10032 <longqicai@gmail.com>
2014-09-29 09:32:42 +0800

    update comments

--- explore.sh
+++ explore.sh
@@ -1,5 +1,8 @@
+# modify in accord with build.sh
 username=misaka10032
 image=pandora
 container=pandora_test
+# point this to your sphinx_root in ABSOLUTE path
 sphinx_root=$PWD/../sphinx
+
 docker run -p 8088:8080 --rm --name $container -v $sphinx_root:/opt/sites/sphinx -t -i $username/$image bash
--- serve.sh
+++ serve.sh
@@ -1,7 +1,8 @@
+# modify in accord with build.sh
 username=misaka10032
 image=pandora
-# you may change this as you like
 container=pandora_server
-# you MUST point this to your sphinx_root
+# point this to your sphinx_root in ABSOLUTE path
 sphinx_root=$PWD/../sphinx
+
 docker run -p 8080:8080 --rm -i -t -v $sphinx_root:/opt/sites/sphinx $username/$image bash -c "supervisord; tail -f /var/log/*.log"
